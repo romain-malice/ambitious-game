2f8bf9169211e12796ee30b5ceadc89edd7a60a6
Romain Malice <romainmalice@gmail.com>
2026-04-18 11:28:35 +0200

    Formatting

--- packages.vhd
+++ packages.vhd
@@ -1,7 +1,7 @@
 ieee library;
 use ieee.numeric_std.all;
-use ieee.std_logic_1164.ALL;
+use ieee.std_logic_1164.all;
 
 package rendering_pkg is
-	constant RENDER_DIST : natural := 500;
+  constant RENDER_DIST : natural := 500;
 end package;
