c61b0977acb5b3ab9d62a58436b2a139fc2d50d8
Romain Malice <romainmalice@gmail.com>
2026-04-18 12:49:30 +0200

    Added background color constant

--- packages.vhd
+++ packages.vhd
@@ -4,4 +4,5 @@ use ieee.std_logic_1164.all;
 
 package rendering_pkg is
   constant RENDER_DIST : natural := 500;
+  constant BKG_COLOR : std_logic_vector := x"8CE"; -- Sky blue
 end package rendering_pkg;
