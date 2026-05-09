afd609d6da81297cfeb0ef676742c9eab7092fb7
francois-zan <fzanella80@gmail.com>
2026-05-09 15:30:51 +0200

    new package + state entity

---
+++ src/game_state.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.math_utils_pkg.all;
+
+entity game_state is
+     port (
+    clk_50 : in std_logic;
+    en : in std_logic;
+    change : 
+
+    state : out game_state_t
+    );
+end entity game_state;
---
+++ src/package.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+package game_package is 
+
+    type game_state_t is (START, SEARCH, WIN, LOOSE)
+
+end package game_package;
